0de9863d4413913e1354ba231d47729f5f9213a8
DKCTC <36381017+DKCTC@users.noreply.github.com>
2026-09-24 10:55:03 -0400

    Updated footer layout

--- examples/react/src/todo/components/footer.jsx
+++ examples/react/src/todo/components/footer.jsx
@@ -27,7 +27,7 @@ export function Footer({ todos, dispatch }) {
                 </li>
             </ul>
             <button
-                className="clear-completed"
+                className="clear-completed-bin"
                 hidden={completedCount === 0}
                 onClick={removeCompleted}
             >
